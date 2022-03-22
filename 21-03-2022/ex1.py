7379bd969585f88b3821a1c6bd1e80ba9c8dab76
Bast-94 <44336050+Bast-94@users.noreply.github.com>
2022-03-22 19:02:07 +0100

    Create ex1.py

---
+++ 21-03-2022/ex1.py
@@ -0,0 +1,18 @@
+#EXERCICE 1
+def edgeset(edges):
+    "Convert a list of undirected edges to a set of directed edges."
+    alledges = set()
+    for (a,b) in edges:
+        alledges.add((a,b))
+        alledges.add((b,a))
+    return alledges
+
+def is_matching(n, edges, matching):
+    alledges = edgeset(edges)
+    matched = [False] * n
+    for (a,b) in matching:
+        if (a,b) not in alledges or matched[a] or matched[b]:
+            return False
+        matched[a] = True
+        matched[b] = True
+    return True
